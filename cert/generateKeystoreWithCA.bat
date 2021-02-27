ac77299c4b7987ff82fbb02407e577676a4800e3
e.kermas@gmail.com <e.kermas@gmail.com>
2021-02-27 17:52:52 +0300

    mtls ok

--- cert/generateKeystoreWithCA.bat
+++ cert/generateKeystoreWithCA.bat
@@ -6,10 +6,20 @@ del ca.key
 del server.jks
 del server.p12
 del server.csr
-del server.crt
 del server.cer
+del server.crt
 del server.key
+del server.pem
 
+del client_1.jks
+del client_1.p12
+del client_1.csr
+del client_1.cer
+del client_1.crt
+del client_1.key
+del client_1.pem
+
+del ..\nginx\ca.crt
 del ..\nginx\server.crt
 del ..\nginx\server.key
 
@@ -30,7 +40,9 @@ keytool -v -importcert -file server.cer -alias server -keystore server.jks -stor
 keytool -importkeystore -srckeystore server.jks -srcstorepass secret -deststorepass secret -keypass secret -destkeystore server.p12 -deststoretype PKCS12
 openssl pkcs12 -in server.p12 -nokeys -out server.crt -password pass:secret -passout pass:secret
 openssl pkcs12 -in server.p12 -nocerts -out server.key -password pass:secret -passout pass:secret
+openssl pkcs12 -in server.p12 -nodes -out server.pem -password pass:secret -passout pass:secret
 
+copy ca.crt ..\nginx\ca.crt
 copy server.crt ..\nginx\server.crt
 copy server.key ..\nginx\server.key
 
@@ -39,10 +51,12 @@ keytool -v -genkeypair -dname "CN=CLIENT_1,OU=Tls,O=Paperbird,C=RU" -keystore cl
 keytool -v -certreq -file client_1.csr -keystore client_1.jks -alias client_1 -keypass secret -storepass secret -keyalg rsa
 keytool -v -gencert -infile client_1.csr -outfile client_1.cer -keystore ca.jks -storepass secret -alias root-ca -ext KeyUsage=digitalSignature,dataEncipherment,keyEncipherment,keyAgreement -ext ExtendedKeyUsage=serverAuth,clientAuth
 keytool -v -importcert -file ca.pem -alias root-ca -keystore client_1.jks -storepass secret -noprompt
-keytool -v -importcert -file client_1.cer -alias server -keystore client_1.jks -storepass secret
+keytool -v -importcert -file client_1.cer -alias client_1 -keystore client_1.jks -storepass secret
+
+keytool -v -delete -alias server -keystore client_1.jks -storepass secret
+keytool -v -delete -alias root-ca -keystore client_1.jks -storepass secret
 
 keytool -importkeystore -srckeystore client_1.jks -srcstorepass secret -deststorepass secret -keypass secret -destkeystore client_1.p12 -deststoretype PKCS12
-openssl pkcs12 -in client_1.p12 -nokeys -out client_1.crt -password pass:secret
-openssl pkcs12 -in client_1.p12 -nocerts -nodes -out client_1.key -password pass:secret
-REM openssl pkcs12 -in client_1/identity.p12 -nodes -out client_1/identity.bundle -password pass:secret
-
+openssl pkcs12 -in client_1.p12 -nokeys -out client_1.crt -password pass:secret -passout pass:secret
+openssl pkcs12 -in client_1.p12 -nocerts -out client_1.key -password pass:secret -passout pass:secret
+openssl pkcs12 -in client_1.p12 -nodes -out client_1.pem -password pass:secret -passout pass:secret
