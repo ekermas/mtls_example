8dc709ef29b50fc4311de59e8f08aa03cfed3959
e.kermas@gmail.com <e.kermas@gmail.com>
2021-02-27 20:43:35 +0300

    tls.nginx ok mtls.nginx ok tls.server ok mtls.server ok

--- server/src/main/java/ru/paperbird/widget/adapter/Boot.java
+++ server/src/main/java/ru/paperbird/widget/adapter/Boot.java
@@ -1,13 +1,53 @@
 package ru.paperbird.widget.adapter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static net.logstash.logback.argument.StructuredArguments.kv;
 
 @SpringBootApplication
-public class Boot {
+@Slf4j
+public class Boot extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Boot.class, args);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+                .and()
+                .x509()
+                .subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+                .userDetailsService(userDetailsService());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                log.debug("{} {}",
+                        kv("method", "loadUserByUsername"),
+                        kv("username", username)
+                );
+                if (username.equals("CLIENT_1")) {
+                    return new User(username, "",
+                            AuthorityUtils
+                                    .commaSeparatedStringToAuthorityList("ROLE_USER"));
+                }
+                throw new UsernameNotFoundException("User not found! " + username);
+            }
+        };
+    }
+    
 }
